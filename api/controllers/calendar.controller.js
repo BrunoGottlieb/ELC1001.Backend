983cc446f63f31fb204b936db583fb1fd06dd4f5
BrunoGottlieb <48501861+BrunoGottlieb@users.noreply.github.com>
2020-06-22 13:58:29 -0300

    22-06-20 - Calendar controller update

--- api/controllers/calendar.controller.js
+++ api/controllers/calendar.controller.js
@@ -3,52 +3,32 @@ const Calendar = require('../models/calendar.model');
 // Dica: você pode usar req.user para acessar informações do usuário que está fazendo a request.
 
 exports.getLook = async (req, res) => {
-
+ 
     try {
-
-        //const look = Look.getById(userId);
-
-        var day = req.params.day;
-        var shift = req.params.shift;
-        var user = req.user.i_id;
-
-        var dt = date.getUTCDate();
-​
-        if (dt < 10) {
-            dt = '0' + dt;
-        }
-
-        const response = serviceDB.getCalendarByDayAndShift (dt, shift, user);
-​​
-        if(response){
-            return res.stats(200).json({ 
-                response 
-            });
-        } else {​
-        
-        if(!response){
-            return res.stats(404).json({ 
-                response
-            });
-        }
-​
-        return res.stats(500).json({ 
-            message: "Not supposed to be here " + req.params.garmentId
-        });
-    }
-}
-        
         // Esse rota deve retornar o look usado pelo usuário no dia (parâmetro day)
         // e turno (parâmetro shift) especificado.
-
         // Se não houver look neste dia, retorne null.
-
         // Preste atenção para padronizar o dia recebido de forma que a hora não influencie
         // o resultado da busca. Use apenas o dia (e.g. 31/12/2000).
-        
         // Pesquise qual deve ser o código de retorno HTTP quando a requisição foi bem sucedida.
 
-    catch(err) {
+        const { _id } = req.user;
+        var { day, shift } = req.params;
+
+        var d = new Date(day);
+        var d = d.toISOString().slice(0,10);
+        
+        const look = await Calendar.find({
+            owner: { $eq: _id },
+            date: { $eq: d },
+            shift: { $eq: shift }
+        });
+        if (!look)
+            return res.status(404);
+
+        return res.status(200).json({ look });
+    }
+    catch (err) {
         console.error(err, err.message, err.stack);
 
         return res.status(500).send({
@@ -58,42 +38,33 @@ exports.getLook = async (req, res) => {
 };
 
 exports.setLook = async (req, res) => {
-    try {
-        
-        //const Calendar= await Calendar.create(req.body); // send body request (request have all informations for create)
-        // return res.json(Calendar);
-        
-        if (calendar.user.i_id != req.user.i_id){
-            return res.stats(409).json({ 
-                calendar 
-            });
-        }
+    
+        // Esse rota deve settar o look usado pelo usuário no dia (parâmetro day)
+        // e turno (parâmetro shift) especificado.
 
-        const response = serviceDB.createCalendar(calendar);
- ​​
-        if(response){
-            return res.stats(201).json({ 
-                response 
-            });
-        } else {​
-         
-            if(!response){
-                return res.stats(400).json({ 
-                    response
-                });
-            }
-    ​
-            return res.stats(500).json({ 
-                message: "Not supposed to be here " + req.params.garmentId
-            })
+        // Preste atenção para garantir que o look recebido realmente pertence ao usuário
+        // que está fazendo a request.
+
+        // Se já houver um look atribuído para esse mesmo dia e turno, sobrescreva-o.
+
+        // Preste atenção para padronizar o dia recebido de forma que a hora não influencie
+        // ou não fique salva no banco. Use apenas o dia (e.g. 31/12/2000).
+
+        // Pesquise qual deve ser o código de retorno HTTP quando a requisição foi bem sucedida.
+        const calendar = new Calendar(req.body);
+
+        console.log(req.body);
+        calendar.owner = req.user._id;
+        try {
+            await calendar.save();
+    
+            res.status(201).send(calendar);
         }
-         
-    }
-    catch(err) {
-        console.error(err, err.message, err.stack);
- ​
-        return res.status(500).send({
-            message: "Error retrieving look for day"
-        });
-    }
-};
+        catch (err) {
+            console.error(err, err.message, err.stack);
+    
+            return res.status(500).send({
+                message: "Error setting look for day"
+            });
+        }
+    };
